17b1e4ead342cfb0d048a282acda8c8895cbcdd1
2862171636 <31610975+2862171636@users.noreply.github.com>
2017-12-02 12:10:01 +0800

    1
    1

---
+++ src/main/java/com/lanou/entity/User.java
@@ -0,0 +1,13 @@
+package com.lanou.entity;
+
+/**
+ * Created by lanou on 2017/12/2.
+ */
+public class User {
+
+    private Integer uId;
+    private String userName;
+    private String password;
+
+
+}
